f963c3cb3d1db5cea583e860d958d9c6e1f99044
ManuelPuchner <manuelpuchner@icloud.com>
2026-05-18 21:06:33 +0200

    auth and deployment changes

--- src/main/java/com/manuelpuchner/backend/BackendApplication.java
+++ src/main/java/com/manuelpuchner/backend/BackendApplication.java
@@ -2,9 +2,10 @@ package com.manuelpuchner.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BackendApplication {
 
